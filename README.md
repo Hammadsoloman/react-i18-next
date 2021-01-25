59297f2b2cae6d7bfd2c4f79e14001e528e4197b
Hammadsoloman <hammadali95@yahoo.com>
2021-01-25 11:20:36 +0200

    add translation

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# react-i18-next
+# i18next React example
+
+Code for [this video](https://youtu.be/txHU6lrsa3o)
---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Trans, useTranslation } from "react-i18next";
+
+function App() {
+  const { t, i18n } = useTranslation();
+
+  const changeLanguage = (language) => {
+    i18n.changeLanguage(language);
+  };
+
+  return (
+    <div className="App">
+      <button onClick={() => changeLanguage("en")}>EN</button>
+      <button onClick={() => changeLanguage("ar")}>AR</button>
+      <hr/>
+      {/* <Trans i18nKey="description.part1">
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </Trans> */}
+       <div>{t("description.part1")}</div>
+      <div>{t("description.part2")}</div>
+    </div>
+  );
+}
+
+export default App;
